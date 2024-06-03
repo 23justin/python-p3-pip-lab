8fbe0fe41d3c7de3cc6e7e817a8302360c05f058
14justin <justinmwangi40@gmail.com>
2024-06-03 23:47:22 +0300

    test

--- lib/versions.py
+++ lib/versions.py
@@ -10,3 +10,12 @@ def requests_version():
 
 def pytest_version():
     return pytest.__version__
+def test_python_version():
+    version_info = python_version()
+    assert version_info.major == 3
+    assert version_info.minor == 10
+def test_requests_version():
+    assert requests_version() == "2.25.1"  
+
+def test_pytest_version():
+    assert pytest_version() == "8.2.1"
